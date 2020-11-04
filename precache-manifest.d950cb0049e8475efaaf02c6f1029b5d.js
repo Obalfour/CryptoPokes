68ddf3e70a08b7f127d2014c0c0466ad7db654af
obalfour <obalfour@itba.edu.ar>
2020-11-04 19:30:33 -0300

    Updates

---
+++ precache-manifest.d950cb0049e8475efaaf02c6f1029b5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674104896ef9bd2128ecdb47b445b3a6",
+    "url": "/CryptoPokes/frontend/index.html"
+  },
+  {
+    "revision": "8516887808f2495062f3",
+    "url": "/CryptoPokes/frontend/static/css/2.d713517f.chunk.css"
+  },
+  {
+    "revision": "213feaed5ffc9c4247c4",
+    "url": "/CryptoPokes/frontend/static/css/main.0621d3cc.chunk.css"
+  },
+  {
+    "revision": "8516887808f2495062f3",
+    "url": "/CryptoPokes/frontend/static/js/2.e2958e3e.chunk.js"
+  },
+  {
+    "revision": "c0713bc4934e1b0aca8afcb2b1757c19",
+    "url": "/CryptoPokes/frontend/static/js/2.e2958e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213feaed5ffc9c4247c4",
+    "url": "/CryptoPokes/frontend/static/js/main.9a608c1b.chunk.js"
+  },
+  {
+    "revision": "7c794d279bd1e19972d4",
+    "url": "/CryptoPokes/frontend/static/js/runtime-main.56156bac.js"
+  },
+  {
+    "revision": "a7cb0fd0a276799dd753a15d479cb7e2",
+    "url": "/CryptoPokes/frontend/static/media/pokesite.a7cb0fd0.png"
+  },
+  {
+    "revision": "749884e0fdd5ca29549c5caad5d3e59a",
+    "url": "/CryptoPokes/frontend/static/media/tutorial1.749884e0.svg"
+  },
+  {
+    "revision": "c7ec3a7c9b5143dea433d47a0d4945a7",
+    "url": "/CryptoPokes/frontend/static/media/tutorial2.c7ec3a7c.svg"
+  },
+  {
+    "revision": "49b760610905d0fa566d04d4cf03f7d6",
+    "url": "/CryptoPokes/frontend/static/media/tutorial3.49b76061.svg"
+  }
+]);
